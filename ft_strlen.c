d36e6c0b3a1d8552d86b1901f459bab9e6255bd3
Kurygan <tylerlover911@Mac-du-Club.local>
2024-10-17 19:13:59 +0200

    modified to suit man functions.

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,11 +6,11 @@
 /*   By: tylerlover911 <tylerlover911@student.42    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/14 14:28:17 by tylerlover9       #+#    #+#             */
-/*   Updated: 2024/10/16 20:28:50 by mkettab          ###   ########.fr       */
+/*   Updated: 2024/10/17 19:13:36 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_strlen(char *str)
+int	ft_strlen(const char *str)
 {
 	int	i;
 
--- libft.h
+++ libft.h
@@ -6,18 +6,20 @@
 /*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 15:08:04 by mkettab           #+#    #+#             */
-/*   Updated: 2024/10/16 18:33:58 by mkettab          ###   ########.fr       */
+/*   Updated: 2024/10/17 19:13:25 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_H
 # define LIBFT_H
 
-int	ft_strlen(char *str);
+int	ft_strlen(const char *str);
 int	ft_isalpha(int c);
 int	ft_isdigit(int c);
 int	ft_isalnum(int c);
 int	ft_isascii(int c);
 int	ft_isprint(int c);
+char	*ft_strchr(const char *str);
+int ft_atoi(const char *str);
 
 #endif
