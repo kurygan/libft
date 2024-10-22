527291e3ddf64f6e9dd835822f033f3387aa362e
Kurygan <tylerlover911@Mac-du-Club.local>
2024-10-23 01:42:13 +0200

    defenitely not working, gg go nest

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/10/17 19:14:34 by mkettab           #+#    #+#             */
-/*   Updated: 2024/10/22 18:41:31ettab          ###   ########.fr       */
+/*   Created: 2024/10/23 01:39:22 by mkettab           #+#    #+#             */
+/*   Updated: 2024/10/23 01:41:52 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,18 +16,16 @@
 char	*ft_strrchr(const char *s, int c)
 {
 	char	*last_occ;
-	unsigned char char_c;
-	
-	char_c = (unsigned char)c;
+
 	last_occ = NULL;
 	while (*s)
 	{
-		if ((char)*s == char_c)
+		if ((char)*s == (char)c)
 			last_occ = (char *)s;
 		s++;
 	}
-	if ((char)*s == char_c)
-		last_occ = (char *)s;	
+	if ((char)*s == (char)c)
+		last_occ = (char *)s;
 	return (last_occ);
 }
 
