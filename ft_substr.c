ecd2f6869ebe7b814b8de7ade58997365f615f68
Kurygan <tylerlover911@Mac-du-Club.local>
2024-10-29 19:03:44 +0100

    fix substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:19:13 by mkettab           #+#    #+#             */
-/*   Updated: 2024/10/29 17:07:58 by mkettab          ###   ########.fr       */
+/*   Updated: 2024/10/29 19:02:20 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,27 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	char	*non_const_s;
 	char	*subbed;
 
+	if (!s)
+		return (NULL);
 	i = 0;
 	non_const_s = (char *)s;
-	subbed = malloc(sizeof(char) * len + 1);
+	if (len > ft_strlen(s))
+		subbed = malloc(sizeof(char) * ft_strlen(s) + 1);
+	else
+		subbed = malloc(sizeof(char) * len + 1);
 	if(!subbed)
 		return (NULL);
-	while(i < start && *non_const_s)
+	while(i < start && non_const_s[i])
 	{
 		i++;
-		non_const_s++;
 	}
-	ft_strlcpy(subbed, non_const_s, len + 1);
+	ft_strlcpy(subbed, &non_const_s[i], len + 1);
 	return (subbed);
 }
 
-/* int main(void)
+int main(void)
 {
-	char *str = "lorem ipsum dolor sit amet";
-	char *dest = ft_substr(str, 0, 10);
+	char *dest = ft_substr("hola", 0, 18446744073709551615);
 	printf("%s\n", dest);
 	return 0;
-} */
+}
