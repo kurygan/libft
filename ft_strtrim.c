28c5f9954525e8dcc81dce79f90dfaddc96f48e5
Kurygan <tylerlover911@Mac-du-Club.local>
2024-10-30 21:11:48 +0100

    strtrim added

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 00:25:29 by mkettab           #+#    #+#             */
-/*   Updated: 2024/10/30 19:02:38 by mkettab          ###   ########.fr       */
+/*   Updated: 2024/10/30 21:10:51 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,9 +18,11 @@ char	*ft_strtrim(char const *s1, char const *set)
 	
 	if(!s1 || !set)
 		return NULL;
+	if(!ft_strlen(s1))
+		return (ft_strdup(""));
 	while(*s1 && ft_strchr(set, *s1))
 		s1++;
-	i = ft_strlen(s1);
+	i = ft_strlen(s1) - 1;
 	while(ft_strchr(set, s1[i]))
 		i--;
 	return (ft_substr(s1, 0, i + 1));
--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:19:13 by mkettab           #+#    #+#             */
-/*   Updated: 2024/10/29 19:14:52 by mkettab          ###   ########.fr       */
+/*   Updated: 2024/10/30 20:11:05 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,15 +26,13 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 	non_const_s = (char *)s;
 	if (ft_strlen(s) <= start)
 		return (ft_strdup(""));
-	if (len > ft_strlen(s))
-		len = ft_strlen(s);
+	if (len > ft_strlen(&s[start]))
+		len = ft_strlen(&s[start]);
 	subbed = malloc(sizeof(char) * len + 1);
 	if (!subbed)
 		return (NULL);
 	while (i < start && non_const_s[i])
-	{
 		i++;
-	}
 	ft_strlcpy(subbed, &non_const_s[i], len + 1);
 	return (subbed);
 }
