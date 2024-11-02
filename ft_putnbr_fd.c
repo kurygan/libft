c3df462836c33a1b1b4d31ebc1b67c58160b2650
Kurygan <tylerlover911@Mac-du-Club.local>
2024-11-02 01:28:06 +0100

    implementing libft in putnbr

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,29 +6,23 @@
 /*   By: mkettab <mkettab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 18:07:57 by mkettab           #+#    #+#             */
-/*   Updated: 2024/11/02 00:11:18 by mkettab          ###   ########.fr       */
+/*   Updated: 2024/11/02 00:18:50 by mkettab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-
-static void	ft_putchar(char c, int fd)
-{
-	write(fd, &c, 1);
-}
+#include "libft.h"
 
 void	ft_putnbr_fd(int n, int fd)
 {
 	if (n == -2147483648)
 	{
-		write (fd, "-2147483648", 11);
+		ft_putstr_fd("-2147483648", fd);
 		return ;
 	}
 	if (n < 0)
 	{
 		n = -n;
-		write(fd, "-", 1);
+		ft_putchar_fd('-', fd);
 	}
 	if (n >= 10)
 	{
@@ -36,5 +30,5 @@ void	ft_putnbr_fd(int n, int fd)
 		ft_putnbr_fd(n % 10, fd);
 	}
 	if (n < 10)
-		ft_putchar(n + '0', fd);
+		ft_putchar_fd(n + '0', fd);
 }
